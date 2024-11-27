628329b7f0daa698ea849a8646b046ce119f8d3a
iveltier <iveltier@mailbox.org>
2024-11-27 22:28:30 +0100

    createt player

--- mmag-main/enemycontroller.js
+++ mmag-main/enemycontroller.js
@@ -16,7 +16,9 @@ export default class EnemyController {
   xVelocity = 0;
   yVelocity = 0;
   defaultXVelocity = 1;
-  defaultXVelocity = 1;
+  defaultYVelocity = 1;
+  moveDownTimerDefault = 30;
+  moveDownTimer = this.moveDownTimerDefault;
 
   constructor(canvas) {
     this.canvas = canvas;
@@ -24,8 +26,25 @@ export default class EnemyController {
   }
 
   draw(ctx) {
+    this.decrementMoveDownTimer();
     this.updateVelocityAndDirection();
     this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
+  }
+
+  resetMoveDownTimer() {
+    if (this.moveDownTimer <= 0) {
+      this.moveDownTimer = this.moveDownTimerDefault;
+    }
+  }
+
+  decrementMoveDownTimer() {
+    if (
+      this.currentDirection === MovingDirection.downLeft ||
+      this.currentDirection === MovingDirection.downRight
+    ) {
+      this.moveDownTimer--;
+    }
   }
 
   updateVelocityAndDirection() {
@@ -33,12 +52,47 @@ export default class EnemyController {
       if (this.currentDirection == MovingDirection.right) {
         this.xVelocity = this.defaultXVelocity;
         this.yVelocity = 0;
+        const rightMostEnemy = enemyRow[enemyRow.length - 1];
+        if (rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+          this.currentDirection = MovingDirection.downLeft;
+          break;
+        }
+      } else if (this.currentDirection == MovingDirection.downLeft) {
+        this.xVelocity = 0;
+        this.yVelocity = this.defaultYVelocity;
+        if (this.moveDown(MovingDirection.left)) {
+          break;
+        }
+      } else if (this.currentDirection === MovingDirection.left) {
+        this.xVelocity = -this.defaultXVelocity;
+        this.yVelocity = 0;
+        const leftMostEnemy = enemyRow[0];
+        if (leftMostEnemy.x <= 0) {
+          this.currentDirection = MovingDirection.downRight;
+          break;
+        }
+      } else if (this.currentDirection === MovingDirection.downRight) {
+        this.xVelocity = 0;
+        this.yVelocity = this.defaultYVelocity;
+        if (this.moveDown(MovingDirection.right)) {
+          break;
+        }
       }
+    }
+  }
+
+  moveDown(newDirection) {
+    this.xVelocity = 0;
+    this.yVelocity = this.defaultYVelocity;
+    if (this.moveDownTimer <= 0) {
+      this.currentDirection = newDirection;
+      return true;
     }
   }
 
   drawEnemies(ctx) {
     this.enemyRows.flat().forEach((enemy) => {
+      enemy.move(this.xVelocity, this.yVelocity);
       enemy.draw(ctx);
     });
   }
--- mmag-main/index.js
+++ mmag-main/index.js
@@ -1,4 +1,5 @@
 import EnemyController from "./enemycontroller.js";
+import Player from "./player.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -10,10 +11,12 @@ const background = new Image();
 background.src = "images/space.jpg";
 
 const enemyController = new EnemyController(canvas);
+const player = new Player(canvas, 3);
 
 function game() {
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   enemyController.draw(ctx);
+  player.draw(ctx);
 }
 
 setInterval(game, 1000 / 60);
