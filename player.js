c0504de3dd9f2c37b9950aea514037ad7bf6be62
iveltier <iveltier@mailbox.org>
2024-12-10 21:59:02 +0100

    touch test

--- player.js
+++ player.js
@@ -18,6 +18,10 @@ export default class Player {
 
     document.addEventListener("keydown", this.keydown);
     document.addEventListener("keyup", this.keyup);
+
+    this.canvas.addEventListener("touchstart", this.touchstart);
+    this.canvas.addEventListener("touchmove", this.touchmove);
+    this.canvas.addEventListener("touchend", this.touchend);
   }
 
   draw(ctx) {
@@ -34,11 +38,12 @@ export default class Player {
     if (this.x < 0) {
       this.x = 0;
     }
-    //right
+    // right
     if (this.x > this.canvas.width - this.width) {
       this.x = this.canvas.width - this.width;
     }
   }
+
   move() {
     if (this.rightPressed) {
       this.x += this.velocity;
@@ -48,27 +53,56 @@ export default class Player {
   }
 
   keydown = (event) => {
-    if (event.code == `ArrowRight` || event.code == `KeyD`) {
+    if (event.code == "ArrowRight" || event.code == "KeyD") {
       this.rightPressed = true;
     }
-    if (event.code == `ArrowLeft` || event.code == `KeyA`) {
+    if (event.code == "ArrowLeft" || event.code == "KeyA") {
       this.leftPressed = true;
     }
-    if (event.code == `Space`) {
+    if (event.code == "Space") {
       this.shootPressed = true;
     }
   };
 
   keyup = (event) => {
-    if (event.code == `ArrowRight` || event.code == `KeyD`) {
+    if (event.code == "ArrowRight" || event.code == "KeyD") {
       this.rightPressed = false;
     }
-    if (event.code == `ArrowLeft` || event.code == `KeyA`) {
+    if (event.code == "ArrowLeft" || event.code == "KeyA") {
       this.leftPressed = false;
     }
-    if (event.code == `Space`) {
+    if (event.code == "Space") {
       this.shootPressed = false;
     }
+  };
+
+  touchstart = (event) => {
+    const touchX =
+      event.touches[0].clientX - this.canvas.getBoundingClientRect().left;
+
+    if (touchX > this.x + this.width / 2) {
+      this.rightPressed = true;
+    } else if (touchX < this.x + this.width / 2) {
+      this.leftPressed = true;
+    }
+  };
+
+  touchmove = (event) => {
+    const touchX =
+      event.touches[0].clientX - this.canvas.getBoundingClientRect().left;
+
+    if (touchX > this.x + this.width / 2) {
+      this.rightPressed = true;
+      this.leftPressed = false;
+    } else if (touchX < this.x + this.width / 2) {
+      this.leftPressed = true;
+      this.rightPressed = false;
+    }
+  };
+
+  touchend = () => {
+    this.rightPressed = false;
+    this.leftPressed = false;
   };
 
   reset() {
